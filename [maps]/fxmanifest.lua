f2ca1fb3326b81cf42612bdc5affa8de8235707e
teixeira212112 <elmamute212112@gmail.com>
2026-02-24 12:15:26 +0000

    postop maps

---
+++ [maps]/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Teixeira212112'
+description 'Portugal Apocalipse Maps'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+-- Streams todas subpastas (hp, pabase, etc.)
+files {
+    'granero/**.*',
+    'hp/**.*',
+    'map/**.*',
+    'motel/**.*',
+    'pabase/**.*',
+    'ymap/**.*'
+}
