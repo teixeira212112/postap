9aca3e284eb2b5d047f5ec4aa8f9f06b13fa61de
teixeira212112 <elmamute212112@gmail.com>
2026-02-27 03:23:39 +0000

    claude

---
+++ claude/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Teixeira212112'
+description 'Bunker Shell - Private Interior Spawn'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/**.*'
+}
