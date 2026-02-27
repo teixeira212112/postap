308941e27e6bc9c125837680f62fce28a2294bd7
teixeira212112 <elmamute212112@gmail.com>
2026-02-27 03:50:35 +0000

    claude 2

---
+++ military_int/fxmanifest.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+files {
+'peds.meta'
+}
+
+game 'gta5'
+fx_version 'cerulean'
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
